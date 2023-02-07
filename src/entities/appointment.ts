bcb5dc9b057fd88ba1a9e5533e891da15741ae65
marianegrao <claranegrao11@gmail.com>
2023-02-06 22:21:43 -0300

    feat: user identity create

--- src/entities/appointment.ts
+++ src/entities/appointment.ts
@@ -1,1 +1,20 @@
-export class Appointment {}
+export interface UserProps {
+  name: string;
+  cpf: string;
+  data_nascimento: Date;
+  telefone: string;
+  email: string;
+  password: string;
+}
+export class User {
+  private props: UserProps;
+
+  get user() {
+    const { password, ...userData } = this.props;
+    return userData;
+  }
+
+  constructor(props: UserProps) {
+    this.props = props;
+  }
+}
